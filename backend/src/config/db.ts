97c86d5676272d50c6de245da5a910e52762ee91
Shawn <karkisandip064@gmail.com>
2025-09-16 11:11:45 +0545

    asd

--- backend/src/config/db.ts
+++ backend/src/config/db.ts
@@ -1,7 +1,6 @@
-import { PrismaClient } from "../generated/prisma";
+// src/config/db.ts
+import { PrismaClient } from "@prisma/client";
 
 const prisma = new PrismaClient();
+
 export default prisma;
-
-
-
