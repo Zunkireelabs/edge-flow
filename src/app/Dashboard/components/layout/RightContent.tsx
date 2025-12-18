13cde692a238a806ed87b71d2c5a75e1e996badd
Zunkireelabs <sthasadin@gmail.com>
2025-12-18 23:13:33 +0545

    fix: Remove navigationItems prop from Header call in RightContent

--- src/app/Dashboard/components/layout/RightContent.tsx
+++ src/app/Dashboard/components/layout/RightContent.tsx
@@ -32,7 +32,7 @@ const navItems: NavigationItem[] = [
 
 const RightContent: React.FC<RightContentProps> = ({ activeView, onViewChange }) => (
   <div className="flex flex-col flex-1 h-full bg-[#f7f7f7] pl-2">
-    <Header activeView={activeView} navigationItems={navItems} />
+    <Header activeView={activeView} />
     <main className="flex-1 overflow-hidden">
       <div className="bg-[#ffffff] rounded-l-xl h-full border border-gray-200 overflow-auto">
         <ContentRouter activeView={activeView} onViewChange={onViewChange} />
